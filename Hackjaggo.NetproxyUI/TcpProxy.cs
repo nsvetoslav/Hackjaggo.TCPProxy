f38593ec9a960909e39d22990865ffe8a243c438
nsvetoslav <svetn001@gmail.com>
2025-01-30 00:27:04 +0200

    remove unused usings

--- Hackjaggo.NetproxyUI/TcpProxy.cs
+++ Hackjaggo.NetproxyUI/TcpProxy.cs
@@ -1,14 +1,9 @@
 ﻿using Hackjaggo.NetproxyUI;
 using NetTools;
-using System;
 using System.Buffers;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hackjaggo.Proxy
 {
